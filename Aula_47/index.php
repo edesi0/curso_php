41ceb8d49ee5f8da253dcb032e9c308e442dfa30
= <edesio@digifarma.com.br>
2025-09-18 14:41:53 -0300

    Desafio 7

---
+++ Aula_47/index.php
@@ -0,0 +1,4 @@
+<?php
+$array_assoc = ['nome'=>'Shark','pastas'=>4];
+print_r($array_assoc);
+print_r($array_assoc['nome']);
---
+++ Desafio 7/index.php
@@ -0,0 +1,9 @@
+<?php
+$pessoa = ['nome'=>'Edésio','idade'=>36];
+
+if ($pessoa['idade']>18){
+    echo('Maior de idade');
+}
+else{
+ echo('Menor de idade');   
+}
