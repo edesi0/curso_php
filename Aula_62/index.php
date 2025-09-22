6e6a9081d15ebbfdcd6cbfa016ed4533e3aaff9f
= <edesio@digifarma.com.br>
2025-09-22 14:31:17 -0300

    Aula e desafios dia 22/09/2025 aula 61 a 63

---
+++ Aula_62/index.php
@@ -0,0 +1,7 @@
+<?php
+
+function teste(){
+    echo("função");
+}
+
+teste();
---
+++ Aula_63/index.php
@@ -0,0 +1,9 @@
+<?php
+$imprimir ="Edesio";
+
+function imprimi_valor(){
+global $imprimir;
+echo ($imprimir);
+}
+
+imprimi_valor();
