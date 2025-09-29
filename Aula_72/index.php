e7e6fc1645f3baa1208f48f662b6be653458719d
= <edesio@digifarma.com.br>
2025-09-29 13:37:18 -0300

    Operações aritimeticas

---
+++ Aula_72/index.php
@@ -0,0 +1,12 @@
+<?php
+$auto_cast = 5.5;
+echo("$auto_cast <br/>");
+
+$auto_cast = $auto_cast-0.5;
+
+echo("gettype($auto_cast) <br/>");
+
+echo(gettype($auto_cast));
+
+
+echo("gettype($auto_cast) <br/>");
---
+++ Aula_74/index.php
@@ -0,0 +1,9 @@
+<?php
+$soma = 5+5;
+echo $soma."<br/>";
+$subtracao = 10-2;
+echo $subtracao."<br/>";
+$multiplicacao = 3*2;
+echo $multiplicacao."<br/>";
+$divisao = 20/4;
+echo $divisao."<br/>";
