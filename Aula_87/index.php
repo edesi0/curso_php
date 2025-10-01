54b478e9d5c22284f673ab52d550761adab7d673
= <edesio@digifarma.com.br>
2025-10-01 14:39:45 -0300

    operadores logicos

---
+++ Aula_87/index.php
@@ -0,0 +1,23 @@
+<?php
+$a = 1;
+$b = "1";
+
+if ($a!==$b){
+    echo("$a e diferente de $b");
+}
+else
+{
+ echo("$a e igual a $b");
+}
+
+echo("<br/>");
+
+$a="1";
+
+if ($a!==$b){
+    echo("$a e diferente de $b");
+}
+else
+{
+ echo("$a e igual a $b");
+}
