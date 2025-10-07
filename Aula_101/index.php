86a359c9f608dd047c616407597a83d1756d28fa
= <edesio@digifarma.com.br>
2025-10-07 14:55:56 -0300

    Ternarios

---
+++ Aula_101/index.php
@@ -0,0 +1,4 @@
+<?php
+$result = (10<20? "Dez e menor que vinte":"Dez e maior que vinte");
+
+echo $result;
---
+++ Desafio_21/index.php
@@ -0,0 +1,10 @@
+<?php
+$a= 10;
+$b = 20;
+
+echo($a>$b?"Variavel A é maior que B":"Variavel B é maior que A <br/>");
+
+$a = 30;
+$b = 10;
+
+echo($a>$b?"Variavel A é maior que B":"Variavel B é maior que A <br/>");
