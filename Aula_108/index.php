f88b9f8a9e76c6d61738f9d27f5a7acc781c2dc5
= <edesio@digifarma.com.br>
2025-10-13 22:57:05 -0300

    estrutura de controle

---
+++ Aula_108/index.php
@@ -0,0 +1,18 @@
+<?php
+$a = 5;
+$b = 6;
+
+if ($a > $b) {
+    echo ("o $a e maior que $b");
+} else {
+    echo ("$b e maior que $b");
+}
+
+$a = 1;
+$b = 0;
+
+if ($a > $b) {
+    echo ("o $a e maior que $b");
+} else {
+    echo ("$b e maior que $b");
+}
