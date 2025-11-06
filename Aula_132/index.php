fa48dca43f68fb50a9508988190a42716b7dad95
= <edesio@digifarma.com.br>
2025-11-05 22:29:57 -0300

    fim estrutura de repetição

---
+++ Aula_132/index.php
@@ -0,0 +1,6 @@
+<?php 
+$nomes = ['Edesio','Paula','De Castro'];
+
+foreach($nomes as $nome){
+  echo("O nome atual é $nome <br/>");
+}
---
+++ Aula_133/index.php
@@ -0,0 +1,5 @@
+<?php
+include('include.php');
+?>
+
+<p>Apos o include</p>
